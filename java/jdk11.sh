1ad62cc39d5510de125c3983722837fb6ea5a682
Sandro Giacomozzi <sandrogiacom@gmail.com>
2018-11-13 10:22:22 -0200

    Update jdk11.sh

--- java/jdk11.sh
+++ java/jdk11.sh
@@ -1,6 +1,4 @@
-sudo apt install openjdk-11-jdk
-echo "set JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/bin/java"
-sudo update-alternatives --config java
-sudo nano /etc/environment
+apt install openjdk-11-jdk
+echo 'JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64"' >> /etc/environment
 source /etc/environment
 echo $JAVA_HOME
