a1da9e52884f3d5f1b8659ebfd7ada4c4a25a78a
Sandro Giacomozzi <sandrogiacom@gmail.com>
2018-11-13 10:34:03 -0200

    Update jdk11.sh

--- java/jdk11.sh
+++ java/jdk11.sh
@@ -1,6 +1,9 @@
 #login as root
+sudo su
 apt remove openjdk*
 apt remove java*
+add-apt-repository ppa:openjdk-r/ppa
+apt-get update
 apt install openjdk-11-jdk
 echo 'JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64"' >> /etc/environment
 source /etc/environment
