a68313a55af99222223820d667d78e7f86b143b6
sandrogiacom <sandrogiacom@gmail.com>
2019-01-08 21:35:07 -0200

    add node install

---
+++ node/node.sh
@@ -0,0 +1,9 @@
+echo ==============================
+echo install node
+echo ==============================
+apt-get install -y nodejs
+
+echo ==============================
+echo install npm
+echo ==============================
+apt-get install -y npm
